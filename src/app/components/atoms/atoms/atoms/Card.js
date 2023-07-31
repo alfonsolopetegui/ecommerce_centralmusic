ae4416a4bfa208e06129db10c8f57c87f412f713
MarAgui04 <101157062+MarAgui04@users.noreply.github.com>
2023-07-30 23:32:16 -0300

    Agregue la seccion new products con sus respectivas cards

---
+++ src/app/components/atoms/atoms/atoms/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'react'
+
+const Card = (props) => {
+  return (
+    <>
+    <figure className='card-container'> 
+      <img src={props.data.img} alt="guitar image" className='card-image'/> 
+      <div className='card-title-container'> 
+      <h6 className='card-title'> {props.data.title} </h6>
+      <h6 className='card-price'> {props.data.price} </h6>
+      </div>
+      <p className='card-description'> {props.data.description} </p>
+      <div className='card-colors-available'> 
+      <p> {props.data.colorsavailable} colors available </p>
+      </div>
+    </figure>
+    </>
+  )
+}
+
+export default Card
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 import styles from "../styles/page.module.css";
 import Head from "next/head";
+import Image from "next/image";
+import Card from "./components/atoms/atoms/atoms/Card";
+import CardList from "./components/molecules/CardList";
 
 export default function Home() {
   return (
@@ -7,6 +10,22 @@ export default function Home() {
       <div>Proyecto React</div>
       <p>Cambios desde nueva rama</p>
       <p>Ahora con nuevo nombre</p>
+      <CardList />
+      {/* <h2> New Products </h2>
+
+<figure className='card-container'> 
+    <Image 
+        src="/Guitar.svg"
+        alt="Guitar"
+        className='card-image'
+        width={314}
+        height={260}
+        priority
+    />
+    <h5 className='card-title'> Brand name </h5>
+    <h5 className='card-price'> $2,995 </h5>
+    <p className='card-description'> Product description here </p>
+</figure> */}
     </>
   );
 }
