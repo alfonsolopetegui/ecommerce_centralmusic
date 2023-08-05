dd6031cd4fc5c29da91a781bed8955d50fb2a854
Rodrigo Santillan <santillanrodrigo234@gmail.com>
2023-08-05 15:51:22 -0300

    modificando

---
+++ src/app/components/atoms/atoms/atoms/LessonCallOut.js
@@ -0,0 +1,20 @@
+import styles from "../../../../../styles/lesson.module.css";
+
+const LessonCallOut = () => {
+  return(
+    <div className="lesson-call-container">
+      <div>FREE GUITAR LESSONS!</div>
+      <div></div>
+      <button>LEARN MORE</button>
+    </div>
+)};
+export default LessonCallOut;
+
+//    <button style={{
+//  color: "white",
+// backgroundColor: "darkgreen",
+//  padding: "10px 20px",
+//  borderRadius: "5px",
+//  fontSize: "1.5rem",
+//  cursor: "pointer"
+//}}>Clickeame</button>);
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 import styles from "../styles/page.module.css";
 import Head from "next/head";
 import CardList from "./components/molecules/CardList";
-import ButtonSmall from "./components/atoms/atoms/atoms/ButtonSmall";
+import LessonCallOut from "./components/atoms/atoms/atoms/LessonCallOut";
 import ArticlesGallery from "./components/molecules/ArticlesGallery";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <ButtonSmall />
-      </div>
+      <LessonCallOut />
       <CardList />
       <ArticlesGallery />
     </>
