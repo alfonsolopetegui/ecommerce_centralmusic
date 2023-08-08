8985e8be658c0dccb9cb79da82aec031dcad53df
Vero Pacheco <m.veronicapacheco@gmail.com>
2023-08-07 21:59:41 -0300

    sacar carrusel

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,14 @@
 import styles from "../styles/page.module.css";
-import styles from '@/styles/Carrousel.module.css'
 import Head from "next/head";
 import CardList from "./components/molecules/CardList";
 import LessonCallOut from "./components/atoms/atoms/atoms/LessonCallOut";
 import ArticlesGallery from "./components/molecules/ArticlesGallery";
-import PopularList from '@/components/PopularList'
-import Carrousel from '@/components/Carrousel'
-import { StyleRegistry } from 'styled-jsx'
 
 export default function Home() {
   return (
     <>
       <LessonCallOut />
       <CardList />
-      <PopularList/>
-      <Carrousel/>
       <ArticlesGallery />
     </>
   );
