e1c0601bc37342cba1ce7b96a7227689b4ce52f4
Alfonso Lopetegui <alfonsolopetegui@hotmail.com>
2023-08-18 19:38:24 -0300

    rutas funcionando en navbar

--- src/app/components/atoms/atoms/atoms/Header.js
+++ src/app/components/atoms/atoms/atoms/Header.js
@@ -1,66 +1,50 @@
-
-import React from 'react';
+import React from "react";
 import styles from "../../../../../styles/header.module.css";
-
+import Link from "next/link";
 
 const Header = () => {
-    const menuItems = [
-        { text: "Guitars", href: "#" },
-        { text: "Accessories", href: "#" },
-        { text: "Storage", href: "#" },
-        { text: "Lessons", href: "#" },
-        { text: "Repairs", href: "#" }
-    ];
+  const menuItems = [
+    { text: "Guitars", href: "/GuitarsPage" },
+    { text: "Accessories", href: "/AccesoriesPage" },
+    { text: "Storage", href: "#" },
+    { text: "Lessons", href: "/LessonsPage" },
+    { text: "Repairs", href: "/repairs" },
+  ];
 
-return (
+  return (
     <div>
-        <header>
-            <div className={styles["nav-container"]}>
+      <header>
+        <div className={styles["nav-container"]}>
+          <div href="#" className={styles["logo"]}>
+            <img src={"Logo.png"} alt="Guitar" />
+            <Link href={"/"}>
+              <h1 className={styles["logo-name"]}>
+                <b>Central</b>Music
+              </h1>
+            </Link>
+          </div>
 
-                <div href="#" className={styles["logo"]}>
+          <div href="#" className={styles["user"]}>
+            <img src={"User.png"} alt="User" />
+          </div>
 
-                <img
-                src={"Logo.png"} 
-                alt="Guitar"/>
-                <h1 className={styles["logo-name"]}><b>Central</b>Music</h1>
+          <div href="#" className={styles["carrito"]}>
+            <img src={"carrito.png"} alt="Carrito" />
+          </div>
 
-                </div>
-
-
-                <div href="#" className={styles["user"]}>
-                
-                <img
-                src={"User.png"} 
-                alt="User"/>
-
-                </div>
-
-
-
-                <div href="#" className={styles["carrito"]}>
-                    
-                <img
-                src={"carrito.png"} 
-                alt="Carrito"/>
-                
-                </div>
-                
-                
-                <nav className={styles["navigation"]}>
-                <ul className={styles["menu"]}> 
-                {menuItems.map(item => (
+          <nav className={styles["navigation"]}>
+            <ul className={styles["menu"]}>
+              {menuItems.map((item) => (
                 <li className={styles["menu-item"]} key={item.text}>
-                <a href={item.href}>{item.text}</a>
+                  <Link href={item.href}>{item.text}</Link>
                 </li>
-                
-                ))}
-                </ul>
-                </nav>
-            </div>
- 
-        </header>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
-);
+  );
 };
 
 export default Header;
