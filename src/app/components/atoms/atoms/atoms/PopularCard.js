5ca9f2247a05bddf825fb1a90440e7da252d3986
Vero Pacheco <m.veronicapacheco@gmail.com>
2023-08-21 19:59:39 -0300

    cambiar button a Add to cart

--- src/app/components/atoms/atoms/atoms/PopularCard.js
+++ src/app/components/atoms/atoms/atoms/PopularCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "../../../../../styles/carrousel.module.css";
+import styles from "../../../../../styles/popularCard.module.css";
 
 
 const PopularCard = (props) => {
@@ -11,8 +11,8 @@ const PopularCard = (props) => {
         <h3 className={styles.popularPrice}>{props.data.price} </h3>
       </div>
       <p className={styles.popularDescription}>{props.data.text}</p>
-      <div className={styles.colorsAvailable}>
-        <p> {props.data.colorsavailable} colours available </p>
+      <div className={styles.buttonConteiner}>
+        <button className={styles.button}>Add To Cart</button>
       </div>
     </figure>
   )
