9016b41e35f59fac0ea1f39a91d730212da78fe9
JPierreAra <jpierreara@gmail.com>
2023-08-23 18:58:56 -0600

    Hice el navbar responsive

--- src/app/components/atoms/atoms/atoms/Header.js
+++ src/app/components/atoms/atoms/atoms/Header.js
@@ -1,55 +1,66 @@
 "use client"
-import React, { useContext } from "react";
-import styles from "../../../../../styles/header.module.css";
+import React, { useContext,useState } from "react";
+// import styles from "../../../../../styles/header.module.css";
 import Link from "next/link";
-import { TotalItems } from "./TotalItems";
+// import { TotalItems } from "./TotalItems";
 import DataContext from "@/app/Context/DataContext";
 
 const Header = () => {
   const { cart } = useContext(DataContext);
 
-  const menuItems = [
-    { text: "Guitars", href: "/GuitarsPage" },
-    { text: "Accessories", href: "/AccesoriesPage" },
-    { text: "Storage", href: "/StoragePage" },
-    { text: "Lessons", href: "/LessonsPage" },
-    { text: "Repairs", href: "/repairs" },
-  ];
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div>
-      <header>
-        <div className={styles["nav-container"]}>
-          <div href="#" className={styles["logo"]}>
-            <img src={"Logo.png"} alt="Guitar" />
-            <Link href={"/"}>
-              <h1 className={styles["logo-name"]}>
-                <b>Central</b>Music
-              </h1>
-            </Link>
-          </div>
-
-          <div href="#" className={styles["user"]}>
-            <img src={"User.png"} alt="User" />
-          </div>
-
-          <Link href={"/Cart"} className={styles["carrito"]}>
-            <img src={"carrito.png"} alt="Carrito" />
-          </Link>
-          {cart.length > 0 && <TotalItems />}
-
-          <nav className={styles["navigation"]}>
-            <ul className={styles["menu"]}>
-              {menuItems.map((item) => (
-                <li className={styles["menu-item"]} key={item.text}>
-                  <Link href={item.href}>{item.text}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </header>
+    <header className="header_container">
+    <div className="nav-container">
+      <div href="#">
+        <Link href={"/"}>
+          <img className="logo" src="Mobile.png"></img>
+        </Link>
+      </div>
+        <nav className="navbar_">
+        
+            <div className={`navbar_Links ${isOpen && "open"}`}>
+                
+                    <Link href="/GuitarsPage" >
+                        Guitars
+                    </Link>
+                
+               
+                    <Link href="/AccesoriesPage">
+                        Accesories
+                    </Link>
+                
+                
+                    <Link href="/StoragePage">
+                        Storage
+                    </Link>
+                
+                    <Link href="LessonsPage">
+                        Lessons
+                    </Link>
+                
+                    <Link href="/repairs">
+                        Repairs
+                    </Link>
+                
+                    <Link href="/Cart">
+                        <img className="cart-icon" src="carrito.png" width={35} height={35}></img>
+                    </Link>
+                    {cart.length > 0 && <TotalItems />}
+                
+                    <Link href="#">
+                        <img className="user-icon" src="User.png" width={35} height={35}></img>
+                    </Link>
+            </div>
+            <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+        </nav>
     </div>
+  </header>
   );
 };
 
