83869bc2d8585f9b65cd7af702541e29eb45cbdd
Alfonso Lopetegui <alfonsolopetegui@hotmail.com>
2023-08-23 23:36:44 -0300

    guitar page funcionando

--- src/app/GuitarsPage/Page.js
+++ src/app/GuitarsPage/Page.js
@@ -1,19 +1,36 @@
 "use client"
 import Card from "../components/atoms/atoms/atoms/Card";
 import styles from "../../styles/guitarsPage.module.css";
-import { GuitarsInfo } from "../components/data/GuitarsInfo";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { DataContext } from "../Context/DataContext";
 import { useContext } from "react";
 
 const GuitarsPage = () => {
-  const { data } = useContext(DataContext);
+  // const { data } = useContext(DataContext);
+
+  const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // URL de la API local
+        const apiUrl = 'http://localhost:5000/products';
+
+        axios.get(apiUrl)
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error('Error al obtener los datos:', error);
+            });
+    }, []);
+
 
   return (
     <>
       <div className={styles.guitarsPageWrapper}>
         <h3 className={styles.titleGuitarPages}>Guitars</h3>
         <div className={styles.guitarContainer}>
-          {data.map((guitar) => {
+          {products.map((guitar) => {
             return <Card key={guitar.id} data={guitar}/>;
           })}
         </div>
--- src/app/components/atoms/atoms/atoms/Header.js
+++ src/app/components/atoms/atoms/atoms/Header.js
@@ -1,66 +1,67 @@
-"use client"
-import React, { useContext,useState } from "react";
+"use client";
+import React, { useContext, useState } from "react";
 // import styles from "../../../../../styles/header.module.css";
 import Link from "next/link";
 // import { TotalItems } from "./TotalItems";
 import DataContext from "@/app/Context/DataContext";
+import { TotalItems } from "./TotalItems";
 
 const Header = () => {
   const { cart } = useContext(DataContext);
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className="header_container">
-    <div className="nav-container">
-      <div href="#">
-        <Link href={"/"}>
-          <img className="logo" src="Mobile.png"></img>
-        </Link>
-      </div>
+      <div className="nav-container">
+        <div href="#">
+          <Link href={"/"}>
+            <img className="logo" src="Mobile.png"></img>
+          </Link>
+        </div>
         <nav className="navbar_">
-        
-            <div className={`navbar_Links ${isOpen && "open"}`}>
-                
-                    <Link href="/GuitarsPage" >
-                        Guitars
-                    </Link>
-                
-               
-                    <Link href="/AccesoriesPage">
-                        Accesories
-                    </Link>
-                
-                
-                    <Link href="/StoragePage">
-                        Storage
-                    </Link>
-                
-                    <Link href="LessonsPage">
-                        Lessons
-                    </Link>
-                
-                    <Link href="/repairs">
-                        Repairs
-                    </Link>
-                
-                    <Link href="/Cart">
-                        <img className="cart-icon" src="carrito.png" width={35} height={35}></img>
-                    </Link>
-                    {cart.length > 0 && <TotalItems />}
-                
-                    <Link href="#">
-                        <img className="user-icon" src="User.png" width={35} height={35}></img>
-                    </Link>
-            </div>
-            <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
+          <div className={`navbar_Links ${isOpen && "open"}`}>
+            <Link href="/GuitarsPage">Guitars</Link>
+
+            <Link href="/AccesoriesPage">Accesories</Link>
+
+            <Link href="/StoragePage">Storage</Link>
+
+            <Link href="LessonsPage">Lessons</Link>
+
+            <Link href="/repairs">Repairs</Link>
+
+            <Link href="/Cart">
+              <img
+                className="cart-icon"
+                src="carrito.png"
+                width={35}
+                height={35}
+              ></img>
+            </Link>
+            {cart.length > 0 && <TotalItems />}
+
+            <Link href="#">
+              <img
+                className="user-icon"
+                src="User.png"
+                width={35}
+                height={35}
+              ></img>
+            </Link>
+          </div>
+          
+          <div
+            className={`nav_toggle ${isOpen && "open"}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </nav>
-    </div>
-  </header>
+      </div>
+    </header>
   );
 };
 
